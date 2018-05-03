d10900c967cf240585dba01a671003d302a1edcd
harikrishna <hari09kg1a0403@gmail.com>
2018-05-02 23:09:59 -0500

    just a simple changes checkup

---
+++ json_tests/json_utils_test.py
@@ -0,0 +1,16 @@
+import unittest
+from json_core.json_utils import JsonUtils
+
+
+class TestJsonUtils(unittest.TestCase):
+
+    def test_is_json_file(self):
+        json_string = "{\"jsonAttribute\": \"jsonValue\"}"
+        self.assertEquals(JsonUtils.is_json_file(json_string), True)
+
+    def test_remove_json_header(self):
+        json_with_header = "header#{\"jsonAttribute\": \"jsonValue\"}"
+        self.assertEquals(JsonUtils.remove_json_header( json_with_header ), "{\"jsonAttribute\": \"jsonValue\"}")
+
+    if __name__ == '__main__':
+        unittest.main()
---
+++ other_stuff/simpleRestApi.py
@@ -0,0 +1,20 @@
+#importing the Flask from flask
+from flask import Flask
+
+app = Flask(__name__)
+
+# the route() takes a path
+# if route("/") then the default path(127.0.0.1:5000) is the method under it
+# if route("/some_path") then 127.0.0.1:5000/some_path get the content from the method
+
+
+@app.route("/")
+@app.route("/hello")
+def hello():
+    return "Welcome to programming"
+
+# the app.run makes the flask start when this file is called
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
